fa1f1fbda05b062dcfd809222d8557a27210bd26
Christain Barnes <cgbarnes@stanford.edu>
2020-01-15 11:30:35 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+           Hello Teach CS+Social Good EDU TEAM!
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
